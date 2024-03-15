86579baefd3b0f055b45dc878a8098cd1cd78e6c
kechalup <kechalup@microsoft.com>
2024-03-15 13:55:45 -0500

    pr comments

--- tests/unit/test_time_series.py
+++ tests/unit/test_time_series.py
@@ -6,25 +6,12 @@ from cdf_fabric_replicator.time_series import TimeSeriesReplicator
 from cognite.client.data_classes.datapoints_subscriptions import (
     DatapointsUpdate,
     Datapoints,
-    DatapointSubscriptionBatch
 )
-from cdf_fabric_replicator.config import SubscriptionsConfig
 from cognite.extractorutils.metrics import BaseMetrics
 from cognite.extractorutils.base import CancellationToken
 import pytest
-from unittest import mock
-from cdf_fabric_replicator.time_series import TimeSeriesReplicator, SubscriptionsConfig, DatapointsUpdate
-from cognite.client import CogniteClient
+from cdf_fabric_replicator.time_series import TimeSeriesReplicator, DatapointsUpdate
 
-
-@pytest.fixture
-def mock_token():
-    yield "test_access_token"
-
-
-@pytest.fixture
-def mock_get_token(mocker, mock_token):
-    yield mocker.patch("cdf_fabric_replicator.time_series.TimeSeriesReplicator.get_token", return_value=mock_token)
 
 @pytest.fixture
 def input_data_not_null():
@@ -39,30 +26,8 @@ def input_data_not_null():
         )]
 
 @pytest.fixture
-def input_data_batch(input_data_not_null):
-    yield DatapointSubscriptionBatch(input_data_not_null, None, False, "test_next_cursor")
-
-@pytest.fixture
 def input_data_null():
     yield []
-
-@pytest.fixture
-def mock_write_deltalake(mocker):
-    yield mocker.patch("cdf_fabric_replicator.time_series.TimeSeriesReplicator.write_pd_to_deltalake", return_value=None)
-
-@pytest.fixture
-def subcription_config():
-    yield SubscriptionsConfig(
-        external_id="test_external_id",
-        partitions=[0],
-        lakehouse_abfss_path_dps="test_lakehouse_abfss_path_dps",
-        lakehouse_abfss_path_ts="test_lakehouse_abfss_path_ts"
-    )
-
-@pytest.fixture
-def mock_send_time_series_to_lakehouse():
-    with mock.patch.object(TimeSeriesReplicator, 'send_time_series_to_lakehouse_table') as mock_method:
-        yield mock_method
 
 
 class TestTimeSeriesReplicator():
