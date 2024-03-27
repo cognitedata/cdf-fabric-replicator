3af680f20abaeac054b0471cf5447bb86de8b35e
Murad Saeter <murad.sater@cognite.com>
2024-03-27 09:29:32 +0100

    Trigger PR run

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -5,7 +5,7 @@ from integration_steps.fabric_steps import assert_timeseries_data_in_fabric
 from integration_steps.service_steps import run_replicator
 
 
-# Test for Timeseries data integration service
+# Test for Timeseries data integration service between CDF and Fabric
 @pytest.mark.parametrize(
     "time_series",
     [
