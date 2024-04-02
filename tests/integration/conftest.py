038cfb004415488e237c0d0dbf596c0ce5023d47
muralidharm <mmuralidhar@microsoft.com>
2024-04-02 03:17:53 -0500

    Data Model Testing Fixtures (#41)
    * initial merge proposal
    
    * Consolidate data generation to acceptance criteria tests, clean up fixtures
    
    * Remove merge conflicts
    
    * Remove unused imports
    
    * Remove outdated environment variables from example and update lakehouse path fixture
    
    * Remove test stubs
    
    * Remove unused imports
    
    * Add Test Replicator (#2)
    
    * Add test replicator, create time series and subscription in fixture, modify dataframes for comparison
    
    * Removed commented code and unused imports
    
    * Fix reference to lakehouse base url, add logic case for timezone aware read from lakehouse
    
    * Add datetime method that works with 3.10, use env variables for table name in config
    
    * Use constant for timestamp column name, update readme and az deploy script for environment variables
    
    * Change name of timeseries back to being more descriptive
    
    * Add TEST_CONFIG_PATH to .env.example and adjust dataframe timestamp adjustment code
    
    * Change dataframe back from loc
    
    * Change timeseries logging line back and use mock for logger
    
    * Add skip to integration test
    
    * Integration Test Framework (#1)
    
    * initial merge proposal
    
    * Consolidate data generation to acceptance criteria tests, clean up fixtures
    
    * Remove merge conflicts
    
    * Remove unused imports
    
    * Remove outdated environment variables from example and update lakehouse path fixture
    
    * Remove test stubs
    
    * Remove unused imports
    
    * Add Test Replicator (#2)
    
    * Add test replicator, create time series and subscription in fixture, modify dataframes for comparison
    
    * Removed commented code and unused imports
    
    * Fix reference to lakehouse base url, add logic case for timezone aware read from lakehouse
    
    * Add datetime method that works with 3.10, use env variables for table name in config
    
    * Use constant for timestamp column name, update readme and az deploy script for environment variables
    
    * Change name of timeseries back to being more descriptive
    
    * Add TEST_CONFIG_PATH to .env.example and adjust dataframe timestamp adjustment code
    
    * Change dataframe back from loc
    
    * Change timeseries logging line back and use mock for logger
    
    * Add skip to integration test
    
    ---------
    
    Co-authored-by: kechalup <kechalup@microsoft.com>
    
    * Add fixtures for data model space, model dml, creating data model in cdf, and cleaning up fabric tables
    
    * Simplify yield statement for space, add helper function for forming lakehouse table names
    
    * Add loaded test config fixture, clear data models from space if space exists
    
    * Replace data model removal with assertion and error message
    
    * Consolidate fixtures, rename table paths fixture, add cognite client fixture to tests
    
    * Restore comment
    
    * Skip test stubs
    
    * Remove interface from graphql
    
    ---------
    
    Co-authored-by: kechalup <kechalup@microsoft.com>
    Co-authored-by: Murad Sæter <51971968+muradsater@users.noreply.github.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,11 +1,22 @@
 import os
 import pytest
+import yaml
 from unittest.mock import Mock
 from azure.identity import DefaultAzureCredential
 from cognite.client import ClientConfig, CogniteClient
 from cognite.client.credentials import OAuthClientCredentials
+from pathlib import Path
+from cognite.client.data_classes.data_modeling import (
+    Space, 
+    SpaceApply,
+    DataModel,
+    View
+)
+from cognite.client.data_classes.data_modeling.ids import DataModelId
 from cognite.extractorutils.base import CancellationToken
 from cognite.extractorutils.metrics import safe_get
+from cognite.extractorutils.base import CancellationToken
+from deltalake.exceptions import TableNotFoundError
 from cdf_fabric_replicator.metrics import Metrics
 from cdf_fabric_replicator.time_series import TimeSeriesReplicator
 from cdf_fabric_replicator.extractor import CdfFabricExtractor
@@ -30,6 +41,16 @@ from tests.integration.integration_steps.time_series_generation import (
 import pandas as pd
 
 load_dotenv()
+RESOURCES = Path(__file__).parent / "resources"
+
+def lakehouse_table_name(table_name:str):
+    return os.environ["LAKEHOUSE_ABFSS_PREFIX"] + "/Tables/" + table_name
+
+@pytest.fixture(scope="session")
+def test_config():
+    with open(os.environ["TEST_CONFIG_PATH"]) as file:
+        config = yaml.safe_load(file)
+    return config
 
 @pytest.fixture(scope="function")
 def test_replicator():
@@ -101,6 +122,57 @@ def time_series(request, cognite_client):
     yield timeseries_set
     remove_time_series_data(timeseries_set, sub_name, cognite_client)
 
+@pytest.fixture(scope="session")
+def test_space(test_config, cognite_client: CogniteClient):
+    space_id = test_config["data_modeling"][0]["space"]
+    space = cognite_client.data_modeling.spaces.retrieve(space_id)
+    if space is None:
+        new_space = SpaceApply(space_id, name="Integration Test Space", description="The space used for integration tests.")
+        space = cognite_client.data_modeling.spaces.apply(new_space)
+    else: # Ensure there aren't existing data models
+        data_model_list = cognite_client.data_modeling.data_models.list(space=space_id)
+        assert len(data_model_list) == 0, "Space should not have existing data models, please remove models/views/containers before testing"
+    yield space
+    cognite_client.data_modeling.spaces.delete(spaces=[space_id])
+    
+@pytest.fixture(scope="function")
+def test_model(cognite_client: CogniteClient, test_space: Space):
+    test_dml = (RESOURCES / "movie_model.graphql").read_text()
+    movie_id = DataModelId(space=test_space.space, external_id="Movie", version="1")
+    created = cognite_client.data_modeling.graphql.apply_dml(
+        id=movie_id, dml=test_dml, name="Movie Model", description="The Movie Model used in Integration Tests"
+    )
+    models = cognite_client.data_modeling.data_models.retrieve(created.as_id(), inline_views=True)
+    yield models.latest_version()
+    cognite_client.data_modeling.data_models.delete(ids=[movie_id])
+    views = models.data[0].views
+    for view in views: # Views and containers need to be deleted so the space can be deleted
+        cognite_client.data_modeling.views.delete((test_space.space, view.external_id, view.version))
+        cognite_client.data_modeling.containers.delete((test_space.space, view.external_id))
+
+@pytest.fixture(scope="function")
+def edge_table_path(test_space: Space, azure_credential: DefaultAzureCredential):
+    edge_table_path = lakehouse_table_name(test_space.space + "_edges")
+    yield edge_table_path
+    try:
+        delta_table = get_ts_delta_table(azure_credential, edge_table_path)
+        delta_table.delete()
+    except TableNotFoundError:
+        print(f"Table not found {edge_table_path}")
+
+@pytest.fixture(scope="function")
+def instance_table_paths(test_model: DataModel[View], azure_credential: DefaultAzureCredential):
+    instance_table_paths = []
+    for view in test_model.views:
+        instance_table_paths.append(lakehouse_table_name(test_model.space + "_" + view.external_id))
+    yield instance_table_paths
+    for path in instance_table_paths:
+        try:
+            delta_table = get_ts_delta_table(azure_credential, path)
+            delta_table.delete()
+        except TableNotFoundError:
+            print(f"Table not found {path}")
+            
 @pytest.fixture()
 def remote_state_store(cognite_client, test_replicator):
     test_replicator._load_state_store()
