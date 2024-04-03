0c5f66aa434adb09f212ce6ad4ac06055354b3e1
Murad Saeter <murad.sater@cognite.com>
2024-04-03 11:56:06 +0200

    delete state store on teardown

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -260,3 +260,9 @@ def raw_time_series(request, azure_credential, cognite_client, test_extractor):
         azure_credential,
         test_extractor.config.source.abfss_prefix + "/" + test_extractor.config.source.raw_time_series_path
     )
+
+    delete_state_store_in_cdf(
+        test_extractor.config.subscriptions, 
+        test_extractor.config.extractor.state_store.raw.database, 
+        test_extractor.config.extractor.state_store.raw.table, 
+        cognite_client)
