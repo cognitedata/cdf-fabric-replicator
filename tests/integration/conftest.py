a5ee7becf3695dee2c628ec55e7342e9974cf2e5
Murad Sæter <51971968+muradsater@users.noreply.github.com>
2024-04-04 09:03:38 +0200

    Data Modeling Executor for Integration Tests (#54)
    * Added Data_Modeling Replicator Fixture for Int tests
    
    * fixed comment for data model runner
    
    ---------
    
    Co-authored-by: kechalup <kechalup@microsoft.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -21,6 +21,7 @@ from deltalake.exceptions import TableNotFoundError
 from cdf_fabric_replicator.metrics import Metrics
 from cdf_fabric_replicator.time_series import TimeSeriesReplicator
 from cdf_fabric_replicator.extractor import CdfFabricExtractor
+from cdf_fabric_replicator.data_modeling import DataModelingReplicator
 from dotenv import load_dotenv
 from tests.integration.integration_steps.cdf_steps import (
     delete_state_store_in_cdf,
@@ -84,6 +85,22 @@ def test_extractor():
     exatractor._load_state_store()
     exatractor.logger = Mock()
     yield exatractor
+    try:
+        os.remove("states.json")
+    except FileNotFoundError:
+        pass
+
+
+
+@pytest.fixture(scope="function")
+def test_data_modeling_replicator():
+    stop_event = CancellationToken()
+    replicator = DataModelingReplicator(metrics=safe_get(Metrics), stop_event=stop_event)
+    replicator._initial_load_config(override_path=os.environ["TEST_CONFIG_PATH"])
+    replicator.cognite_client = replicator.config.cognite.get_cognite_client(replicator.name)
+    replicator._load_state_store()
+    replicator.logger = Mock()
+    yield replicator
     try:
         os.remove("states.json")
     except FileNotFoundError:
--- tests/integration/integration_steps/service_steps.py
+++ tests/integration/integration_steps/service_steps.py
@@ -1,6 +1,7 @@
 from cdf_fabric_replicator.time_series import TimeSeriesReplicator
 from cdf_fabric_replicator.extractor import CdfFabricExtractor
 from pandas import DataFrame
+from cdf_fabric_replicator.data_modeling import DataModelingReplicator
 
 def run_replicator(test_replicator: TimeSeriesReplicator):
     # Processes data point subscription batches
@@ -11,6 +12,10 @@ def run_extractor(test_extractor: CdfFabricExtractor, data_frame: DataFrame):
     # Processes data point subscription batches
     test_extractor.write_time_series_to_cdf(data_frame)
 
+
+def run_data_modeling_replicator(test_data_modeling_replicator: DataModelingReplicator):
+    # Processes spaces for data-modeling changes
+    test_data_modeling_replicator.process_spaces()
 
 def start_replicator():
     # Start the replicator service
