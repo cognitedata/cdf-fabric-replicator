6e3a4f6ebfe9e7186fa10741a623ca4735130d93
Maya Muralidhar <mmuralidhar@microsoft.com>
2024-04-10 11:28:22 -0500

    Add event size for case where events aren't divisible by batch size

--- tests/integration/test_events_replicator.py
+++ tests/integration/test_events_replicator.py
@@ -50,7 +50,7 @@ def events_path(azure_credential: DefaultAzureCredential):
     delete_delta_table_data(azure_credential, path)
 
 # The parameterized fixtures will be executed for each parameter combination
-@pytest.mark.parametrize("event_write_list", [10, 100], indirect=True,) # Number of events to be created in CDF
+@pytest.mark.parametrize("event_write_list", [10, 25, 100], indirect=True,) # Number of events to be created in CDF
 @pytest.mark.parametrize("batch_size", [10, 100]) # Batch size for the replicator
 @pytest.mark.skip("Skip test: Events Replicator not yet implemented", allow_module_level=True)
 def test_events_service(cognite_client, azure_credential, batch_size, event_write_list, events_dataframe, events_path):
