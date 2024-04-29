4df6c2922a2fccbb9bb65a7edc7c552c646912c1
kechalup <kechalup@microsoft.com>
2024-04-29 08:19:08 -0500

    adding csi driver

--- cdf_fabric_replicator/__main__.py
+++ cdf_fabric_replicator/__main__.py
@@ -15,23 +15,23 @@ def main() -> None:
     stop_event = CancellationToken()
     worker_list = []
 
-    with EventsReplicator(
-        metrics=safe_get(Metrics), stop_event=stop_event
-    ) as event_replicator:
-        worker_list.append(threading.Thread(target=event_replicator.run))
+    # with EventsReplicator(
+    #     metrics=safe_get(Metrics), stop_event=stop_event
+    # ) as event_replicator:
+    #     worker_list.append(threading.Thread(target=event_replicator.run))
 
-    with TimeSeriesReplicator(
-        metrics=safe_get(Metrics), stop_event=stop_event
-    ) as ts_replicator:
-        worker_list.append(threading.Thread(target=ts_replicator.run))
+    # with TimeSeriesReplicator(
+    #     metrics=safe_get(Metrics), stop_event=stop_event
+    # ) as ts_replicator:
+    #     worker_list.append(threading.Thread(target=ts_replicator.run))
 
-    with CdfFabricExtractor(stop_event=stop_event) as extractor:
-        worker_list.append(threading.Thread(target=extractor.run))
+    # with CdfFabricExtractor(stop_event=stop_event) as extractor:
+    #     worker_list.append(threading.Thread(target=extractor.run))
 
-    with DataModelingReplicator(
-        metrics=safe_get(Metrics), stop_event=stop_event
-    ) as dm_replicator:
-        worker_list.append(threading.Thread(target=dm_replicator.run))
+    # with DataModelingReplicator(
+    #     metrics=safe_get(Metrics), stop_event=stop_event
+    # ) as dm_replicator:
+    #     worker_list.append(threading.Thread(target=dm_replicator.run))
 
     for worker in worker_list:
         worker.start()
