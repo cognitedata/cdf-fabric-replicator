dd8334a0e00742b5e1874fa6dd0618953c615229
Nick Perkins <niperkin@microsoft.com>
2024-05-08 11:49:21 -0400

    Prehook commit, removed http probes

--- tests/integration/test_helm.py
+++ tests/integration/test_helm.py
@@ -2,8 +2,9 @@ import subprocess
 from time import sleep
 import pytest
 
+
 @pytest.fixture(scope="function")
-def helm_setup_teardown(request):
+def release_name(request):
     release_name = request.param
     print("Uninstalling the Helm chart if it is already installed...")
     uninstall_helm_chart(release_name)
@@ -11,24 +12,30 @@ def helm_setup_teardown(request):
     print("Uninstalling the Helm chart")
     uninstall_helm_chart(release_name)
 
+
 def uninstall_helm_chart(release_name):
     uninstall_command = f"helm uninstall {release_name}"
     subprocess.run(uninstall_command, shell=True)
-    
+
+
 def install_helm_chart(release_name, chart_name):
     install_command = f"helm install {release_name} {chart_name}"
     subprocess.run(install_command, shell=True, check=True)
-    
+
+
 def check_pod_status():
     kube_status_command = "kubectl get pods"
-    kube_status_output = subprocess.check_output(kube_status_command, shell=True).decode("utf-8")
+    kube_status_output = subprocess.check_output(
+        kube_status_command, shell=True
+    ).decode("utf-8")
     print(kube_status_output)
     return kube_status_output
+
 
 def wait_for_pod_to_start(retries):
     for i in range(retries):
         sleep(2**i)  # wait before the next retry using exponential backoff
-        
+
         kube_status_output = check_pod_status()
 
         if "Running" in kube_status_output:
@@ -38,7 +45,8 @@ def wait_for_pod_to_start(retries):
     else:
         # If we've reached this point, the pod is not up after the maximum number of retries
         assert False, "Pod did not start up within the expected time"
-        
+
+
 def assert_pod_stays_running(running_time):
     for _ in range(running_time):
         sleep(1)  # wait for 1 second
@@ -46,27 +54,26 @@ def assert_pod_stays_running(running_time):
         kube_status_output = check_pod_status()
 
         assert "Running" in kube_status_output, "Pod did not stay running"
-        
+
+
 def assert_helm_deployed(release_name):
     # Check if the release is deployed successfully
     status_command = f"helm status {release_name}"
     status_output = subprocess.check_output(status_command, shell=True).decode("utf-8")
     assert "STATUS: deployed" in status_output, "Helm chart deployment failed"
 
+
 @pytest.mark.skip(reason="Skipping as this test requires an AKS cluster.")
-@pytest.mark.parametrize('helm_setup_teardown', ["int-test-helm"], indirect=True)
+@pytest.mark.parametrize("helm_setup_teardown", ["int-test-helm"], indirect=True)
 def test_helm_chart_deployment(helm_setup_teardown, retries=5, running_time=60):
     # Set the Helm chart name and release name
     chart_name = "cdf-fabric-replicator-chart"
-    release_name = helm_setup_teardown
-    
     # Install the Helm chart
-    install_helm_chart(release_name, chart_name)
+    install_helm_chart(helm_setup_teardown, chart_name)
 
-    assert_helm_deployed(release_name)
-    
+    assert_helm_deployed(helm_setup_teardown)
+
     wait_for_pod_to_start(retries)
-        
+
     # Check that the pod stays running for the specified running time
     assert_pod_stays_running(running_time)
-
